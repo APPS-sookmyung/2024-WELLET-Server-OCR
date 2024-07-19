96a32a4d932b94ffa806ed3f5aca77abceb8f924
ajeong <ajung7038@naver.com>
2024-07-19 20:15:35 +0900

    Feat: ocr 데이터 추출 구현 #1

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
 
+import sys, os
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+from pages import train_data
+import json
+
 app = FastAPI()
 
 @app.get("/health")
 def root():
     return {"message" : "WELLET Server OCR Health Check"}
+
+@app.get("/ocr")
+def ocr():
+    result = {'result': train_data.ocrService()}
+    return result
+    
--- pages/train_data.py
+++ pages/train_data.py
@@ -4,14 +4,16 @@ import matplotlib.pyplot as plt
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
 
-path = os.getenv("FILE_PATH")
+def ocrService():
+    load_dotenv()
+    path = os.getenv("FILE_PATH")
 
-pytesseract.pytesseract.tesseract_cmd = os.getenv("PYTESSERACT_PATH")
+    pytesseract.pytesseract.tesseract_cmd = os.getenv("PYTESSERACT_PATH")
 
-image = cv2.imread(path)
-rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image = cv2.imread(path)
+    rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-text = pytesseract.image_to_string(rgb_image, lang='kor+eng')
-print(text)
+    text = pytesseract.image_to_string(rgb_image, lang='kor+eng')
+    return text
+
